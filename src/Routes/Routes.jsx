48e630afde96ad50e206e07d393209dbad20e3f9
unknown <muttakinahmed871@gmail.com>
2023-06-09 23:20:30 +0600

    basic router setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,26 @@
+import {
+    createBrowserRouter,
+   
+  } from "react-router-dom"
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Error from "../pages/Error/Error";
+
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+      ]
+    },
+    {
+        path:'*',
+        element:<Error></Error>
+    }
+  ]);
+
+  
