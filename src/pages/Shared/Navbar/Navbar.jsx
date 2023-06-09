3ada8194739e72530addd9eab98ba27b17ef5546
unknown <muttakinahmed871@gmail.com>
2023-06-10 00:12:33 +0600

    add logo in navbar

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import logo from '../../../assets/logo.png'
 
 
 const Navbar = () => {
@@ -20,16 +20,16 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-xl">
                         {
                             navItems
                         }
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">Learn Language</a>
+                 <img  src={logo} alt="" />
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 text-xl">
                     {navItems}
                 </ul>
             </div>
