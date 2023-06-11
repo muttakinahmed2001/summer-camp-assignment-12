544de54de1fe70c3ff35c0c0530e8727f157b1cf
unknown <muttakinahmed871@gmail.com>
2023-06-11 17:36:20 +0600

    implement error validation

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,11 +2,25 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { FcGoogle } from "react-icons/fc";
 import { IconContext } from "react-icons";
+import { useState } from 'react';
 
 const SignUp = () => {
+  const [error,setError]= useState();
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(errors);
+    const onSubmit = data => {
+    const password = data.password;
+    const confirmPassword = data.confirmPassword;
+   
+
+    if(password !== confirmPassword){
+   
+    setError('Password does not matched')
+    return;
+   }
+    setError('')
+   console.log(data)  
+    };
+    
     return (
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-col">
@@ -28,8 +42,8 @@ const SignUp = () => {
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
-                <input type="text" placeholder="Name"  {...register("name", { required: true })} className="input input-bordered" />
-                {errors.name && <span>Name is required</span>}
+                <input type="text" placeholder="Name"  {...register("name")} className="input input-bordered" />
+                
   
   
               </div>
@@ -37,8 +51,8 @@ const SignUp = () => {
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" placeholder="email"  {...register("name", { required: true })} className="input input-bordered" />
-                {errors.name && <span>Email is required</span>}
+                <input type="email" placeholder="email"  {...register("email", { required: true })} className="input input-bordered" />
+                {errors.email && <span>Email is required</span>}
   
   
               </div>
@@ -46,34 +60,36 @@ const SignUp = () => {
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <div className="flex items-center"> <input type='password' name="password" placeholder={"password"} {...register("password", { required: true })} className="input input-bordered" />
+                <div className="flex items-center"> <input type='password' name="password" placeholder={"password"} {...register("password", { required: true, minLength: 6,
+                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])/})} className="input input-bordered" />
   
                    
                 </div>
-                {errors.password && <span>Password is required</span>}
-                  
+                {errors.password?.type === 'required' && <span className='text-red-600'>Password is required</span>}
+                {errors.password?.type ==='minLength' && <span className='text-red-600'>Password must be 6 characters</span>}
+                {errors.password?.type ==='pattern' && <span className='text-red-600'>Password must have one capital letter and one special character</span>}
+                  <p></p>
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Password</span>
+                  <span className="label-text">Confirm password</span>
                 </label>
-                <div className="flex items-center"> <input type='password' name="confirmPassword" placeholder={"confirmPassword"} {...register("confirmPassword", { required: true })} className="input input-bordered" />
-  
-                   
+                <div className="flex items-center"> <input type='password' name="confirmPassword" placeholder={"Confirm password"} {...register("confirmPassword", { required: true })} className="input input-bordered" />
+   
                 </div>
-                {errors.password && <span>Password is required</span>}
                 
+                <p className='text-red-600'>{error}</p>
               </div>
                
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Photo URL</span>
                 </label>
-                <div className="flex items-center"> <input type='text' name="photo" placeholder={"Photo URL"} {...register("photo", { required: true })} className="input input-bordered" />
+                <div className="flex items-center"> <input type='text' name="photo" placeholder={"Photo URL"} {...register("photo" )} className="input input-bordered" />
   
                    
                 </div>
-                {errors.password && <span>Password is required</span>}
+                
                  <p className='mt-2'>Already have an account? <Link to='/login'>Login </Link> </p>
               </div>
                
