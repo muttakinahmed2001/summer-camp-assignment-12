fe3befd4f07123c00d51cef85b1e98b0f49a9ea6
unknown <muttakinahmed871@gmail.com>
2023-06-12 18:12:00 +0600

    apply action of making admin and  instructor

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,19 +1,91 @@
 import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
- 
+
 
 const AllUsers = () => {
-    const {data: users =[], refetch} = useQuery(['users'],async()=>{
-         
-        const res = await fetch('http://localhost:5000/users')
-        return res.json();
-    })
-    return (
-        <div>
-           
-             <h1 className="text-2xl font-bold">Total Users:{users.length}</h1>
-        </div>
-    );
+  const { data: users = [], refetch } = useQuery(['users'], async () => {
+
+    const res = await fetch('http://localhost:5000/users')
+    return res.json();
+  })
+
+  const handleMakeAdmin = id => {
+    fetch(`http://localhost:5000/users/admin/${id}`,{
+      method: 'PATCH'
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    if(data.modifiedCount >0){
+      refetch()
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Role changed successfully',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
+  }
+  )
+  }
+  const handleMakeInstructor = id => {
+    fetch(`http://localhost:5000/users/instructor/${id}`,{
+      method: 'PATCH'
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    if(data.modifiedCount >0){
+      refetch()
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Role changed successfully',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
+  }
+  )
+  }
+
+  return (
+    <div className="w-full">
+
+      <h1 className="text-2xl font-bold my-4">Total Users: {users.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Roll</th>
+              <th>Make Admin</th>
+              <th>Make Instructor</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((user, index) => <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.role === 'admin' ? 'Admin' : user.role ==='instructor' ? 'Instructor' : 'Student' }</td>
+                <td><button onClick={() =>handleMakeAdmin(user._id)} className="btn btn-accent">Make Admin</button></td>
+                <td><button onClick={() =>handleMakeInstructor(user._id)} className="btn btn-info">Make Instructor</button></td>
+              </tr>
+              )
+            }
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
