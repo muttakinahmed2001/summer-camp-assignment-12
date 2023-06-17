4f7c7ad8bf0c87efbb5169ed1999a0666667d99d
unknown <muttakinahmed871@gmail.com>
2023-06-17 16:46:11 +0600

    some change

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,24 +1,29 @@
+ 
 import Animation from "../Animation/Animation";
 import PopularInstructors from "../PopularInstructors/PopularInstructors";
 import Carousal from "../Slider/Carousal";
 
- 
 const Home = () => {
-    const arabic = "arabic"
-    const mandarin = "mandarin"
-    const language = "english";
+   
+  const arabic = "arabic";
+  const mandarin = "mandarin";
+  const language = "english";
 
+  
   return (
     <div className="mx-auto">
-      <Carousal />
-      <PopularInstructors />
+      
 
-      <Animation language={arabic} />
-      <Animation language={language} />
-      <Animation language={mandarin} />
+      <div>
+        <Carousal />
+        <PopularInstructors />
+
+        <Animation language={arabic} />
+        <Animation language={language} />
+        <Animation language={mandarin} />
+      </div>
     </div>
   );
 };
-
 
 export default Home;
